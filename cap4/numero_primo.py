1f134b683b3f41ef2827eea121209ecde28f63d8
Red Bill <redbillb@gmail.com>
2018-07-23 20:02:00 -0300

    atualizando exercicio de numeros primos

--- cap4/numero_primo.py
+++ cap4/numero_primo.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#imports
-
-
 #processamento
 def divisores(numero):
     lista = []
@@ -11,13 +8,34 @@ def divisores(numero):
     		lista.append(contador)
     return lista
 
-
 def ehPrimo(numero):
     lista = divisores(numero)
     if len(lista) == 2:
         return lista
 
 #testes
+assert ehPrimo(2)
+assert ehPrimo(3)
 assert ehPrimo(5)
+assert ehPrimo(7)
+assert ehPrimo(11)
+assert ehPrimo(13)
+assert ehPrimo(17)
+assert ehPrimo(19)
+assert ehPrimo(23)
+
+assert not ehPrimo(0)
+assert not ehPrimo(1)
 assert not ehPrimo(4)
-assert ehPrimo(97)
+assert not ehPrimo(6)
+assert not ehPrimo(8)
+assert not ehPrimo(9)
+assert not ehPrimo(10)
+assert not ehPrimo(12)
+assert not ehPrimo(14)
+assert not ehPrimo(15)
+assert not ehPrimo(16)
+assert not ehPrimo(18)
+
+assert not ehPrimo(-2)
+assert not ehPrimo(-4)
