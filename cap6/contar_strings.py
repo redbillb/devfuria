d042359aebf74add866bb98ee9ef6710d6d85dd7
Red Bill <redbillb@gmail.com>
2018-07-24 21:26:15 -0300

    Exercicio para contar caracteres em uma string

---
+++ cap6/contar_strings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+#imports
+
+
+#processamento
+def contarStrings(string):
+	return len(string)
+
+#testes
+assert 8 == contarStrings("Red Bill")
+assert 0 == contarStrings("")
