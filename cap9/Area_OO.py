72d7c3a74663bf8d66f3d9f279b3f270b44f8105
Red Bill <redbillb@gmail.com>
2018-08-24 20:24:17 -0300

    ajuste na forma de claculo

--- cap9/Area_OO.py
+++ cap9/Area_OO.py
@@ -7,12 +7,11 @@ import unittest
 #processamento
 
 class Area(object):
-	def areaQuadrada(self,numero):
-		return numero * numero
+	def areaQuadrada(self,numero1,numero2):
+		return numero1 * numero2
 
-	def areaCubica(self,numero):
-		return numero * numero * numero
-
+	def areaCubica(self,numero1,numero2,numero3):
+		return numero1 * numero2 * numero3
 
 #Testes
 
@@ -21,14 +20,14 @@ class testeAreas(unittest.TestCase):
 	area = Area()
 
 	def testeAreaQuadrada(self):
-		self.assertEqual(16,self.area.areaQuadrada(4))
-		self.assertEqual(25,self.area.areaQuadrada(5))
-		self.assertNotEqual(17,self.area.areaQuadrada(4))
+		self.assertEqual(16,self.area.areaQuadrada(4,4))
+		self.assertEqual(25,self.area.areaQuadrada(5,5))
+		self.assertNotEqual(17,self.area.areaQuadrada(4,3))
 
 	def testeAreaCubica(self):
-		self.assertEqual(8,self.area.areaCubica(2))
-		self.assertEqual(27,self.area.areaCubica(3))
-		self.assertNotEqual(9,self.area.areaCubica(2))
+		self.assertEqual(8,self.area.areaCubica(2,2,2))
+		self.assertEqual(27,self.area.areaCubica(3,3,3))
+		self.assertNotEqual(9,self.area.areaCubica(2,3,4))
 
 if __name__ == '__main__':
 	unittest.main()
