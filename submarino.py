fb7b82e088678c52aa0c148109005e8fcdfb1815
Red Bill <redbillb@gmail.com>
2018-08-23 23:27:29 -0300

    Refatorando os testes com base nos testes do Flavio!

--- submarino.py
+++ submarino.py
@@ -1,111 +1,188 @@
 # -*- coding: utf-8 -*-
+import unittest
 
 class Submarino:
 
-	def __init__(self):
-		self.X = 0
-		self.Y = 0
-		self.Z = 0
-		self.direcao = 'NORTE'
-		self.direcoes=['NORTE','LESTE','SUL','OESTE']
+    def __init__(self):
+        self.X = 0
+        self.Y = 0
+        self.Z = 0
+        self.direcao = 'NORTE'
+        self.direcoes=['NORTE','LESTE','SUL','OESTE']
 
-	def ressetaCoordenadas(self):
-		self.__init__()
-		return f'{self.X} {self.Y} {self.Z} {self.direcao}'
+    def ressetaCoordenadas(self):
+        self.__init__()
+        return f'{self.X} {self.Y} {self.Z} {self.direcao}'
 
-	def calculaCoordenada(self,comandos,resseta):
-		if resseta:
-			self.ressetaCoordenadas()
-		for caractere in comandos:
-			if caractere == 'L':
-				self.calculaLEFT()
+    def calculaCoordenada(self,comandos,resseta):
+        if resseta:
+            self.ressetaCoordenadas()
+        for caractere in comandos:
+            if caractere == 'L':
+                self.calculaLEFT()
 
-			elif caractere == 'R':
-				self.calculaRIGTH()
+            elif caractere == 'R':
+                self.calculaRIGTH()
 
-			elif caractere == 'U':
-				self.calculaUP()
+            elif caractere == 'U':
+                self.calculaUP()
 
-			elif caractere == 'D':
-				self.calculaDOWN()
+            elif caractere == 'D':
+                self.calculaDOWN()
 
-			elif caractere == 'M':
-				self.calculaAvanco()
+            elif caractere == 'M':
+                self.calculaAvanco()
 
-			else:
-				pass
+            else:
+                pass
 
-		return f'{self.X} {self.Y} {self.Z} {self.direcao}'
+        return f'{self.X} {self.Y} {self.Z} {self.direcao}'
 
-	def calculaIndiceDirecaoAtual(self):
-		for indice in range(len(self.direcoes)):
-			if self.direcao == self.direcoes[indice]:
-				return indice
+    def calculaIndiceDirecaoAtual(self):
+        for indice in range(len(self.direcoes)):
+            if self.direcao == self.direcoes[indice]:
+                return indice
 
-	def calculaLEFT(self):
-		indiceDirecoes = self.calculaIndiceDirecaoAtual() + 1
-		indiceDirecoes = self.ajustaIndiceDirecoes(indiceDirecoes)
-		self.direcao = self.direcoes[indiceDirecoes]
-		return self.direcao
+    def calculaLEFT(self):
+        indiceDirecoes = self.calculaIndiceDirecaoAtual() - 1
+        indiceDirecoes = self.ajustaIndiceDirecoes(indiceDirecoes)
+        self.direcao = self.direcoes[indiceDirecoes]
+        return self.direcao
 
-	def calculaRIGTH(self):
-		indiceDirecoes = self.calculaIndiceDirecaoAtual() - 1
-		indiceDirecoes = self.ajustaIndiceDirecoes(indiceDirecoes)
-		self.direcao = self.direcoes[indiceDirecoes]
-		return self.direcao
+    def calculaRIGTH(self):
+        indiceDirecoes = self.calculaIndiceDirecaoAtual() + 1
+        indiceDirecoes = self.ajustaIndiceDirecoes(indiceDirecoes)
+        self.direcao = self.direcoes[indiceDirecoes]
+        return self.direcao
 
-	def ajustaIndiceDirecoes(self,indice):
-		if indice == -1:
-			indice = 3
+    def ajustaIndiceDirecoes(self,indice):
+        if indice == -1:
+            indice = 3
 
-		if indice == 4:
-			indice = 0
+        if indice == 4:
+            indice = 0
 
-		return indice
-		
-	def calculaUP(self):
-		if self.Z < 0:
-			self.Z += 1
-		return self.Z
-		
-	def calculaDOWN(self):
-		self.Z -= 1
-		return self.Z
-		
-	def calculaAvanco(self):
-		if self.direcao == 'NORTE':
-			self.Y += 1
-			return self.Y
-		
-		if self.direcao == 'SUL':
-			self.Y -= 1
-			return self.Y
+        return indice
+        
+    def calculaUP(self):
+        if self.Z < 0:
+            self.Z += 1
+        return self.Z
+        
+    def calculaDOWN(self):
+        self.Z -= 1
+        return self.Z
+        
+    def calculaAvanco(self):
+        if self.direcao == 'NORTE':
+            self.Y += 1
+            return self.Y
+        
+        if self.direcao == 'SUL':
+            self.Y -= 1
+            return self.Y
 
-		if self.direcao == 'LESTE':
-			self.X -= 1
-			return self.X
-		
-		if self.direcao == 'OESTE':
-			self.X += 1
-			return self.X
+        if self.direcao == 'LESTE':
+            self.X += 1
+            return self.X
+        
+        if self.direcao == 'OESTE':
+            self.X -= 1
+            return self.X
 
 #testes
-#OBS: a cada teste a posição volta a ser a inicial
 
 submarino = Submarino()
-assert '0 0 0 NORTE' == submarino.calculaCoordenada('',True)
-assert '0 0 0 NORTE' == submarino.calculaCoordenada('a',True)
-assert '0 0 0 LESTE' == submarino.calculaCoordenada('L',True)
-assert '0 0 0 OESTE' == submarino.calculaCoordenada('R',True)
-assert '0 0 0 NORTE' == submarino.calculaCoordenada('U',True)
-assert '0 0 -1 NORTE' == submarino.calculaCoordenada('D',True)
-assert '0 0 -4 NORTE' == submarino.calculaCoordenada('DDDD',True)
-assert '0 0 0 NORTE' == submarino.calculaCoordenada('DDDUUU',True)
-assert '0 0 -2 NORTE' == submarino.calculaCoordenada('UUUUDD',True)
-assert '0 1 0 NORTE' == submarino.calculaCoordenada('M',True)
-assert '0 5 0 NORTE' == submarino.calculaCoordenada('MMMMM',True)
-assert '0 -4 0 SUL' == submarino.calculaCoordenada('RRMMMM',True)
-assert '0 0 0 NORTE' == submarino.calculaCoordenada('RRRR',True)
-assert '0 0 0 NORTE' == submarino.calculaCoordenada('LLLL',True)
 
-assert '2 3 -2 SUL' == submarino.calculaCoordenada('RMMLMMMDDLL',True)
+class TestSubmarino(unittest.TestCase):
+    
+    def testeSugeridoNoEnunciado(self):
+        self.assertEqual(
+            '2 3 -2 SUL',submarino.calculaCoordenada('RMMLMMMDDLL',True)
+            )
+
+        self.assertEqual(
+            '-1 2 0 NORTE',submarino.calculaCoordenada('LMRDDMMUU',True)
+            )
+
+    # 
+    #      NORTE
+    #        0
+    #
+    #
+    #OESTE         LESTE
+    #  3             1
+    #
+    #
+    #       2
+    #      SUL
+    #
+    #  NORTE - SUL eixo Y
+    #  OESTE - LESTE eixo X
+
+
+    def testeDeRotacao(self):
+        submarino.ressetaCoordenadas()
+        self.assertEqual('NORTE', submarino.direcao)
+        self.assertEqual('OESTE', submarino.calculaLEFT())
+        self.assertEqual('NORTE', submarino.calculaRIGTH())
+        self.assertEqual('LESTE', submarino.calculaRIGTH())
+        self.assertEqual('SUL', submarino.calculaRIGTH())
+
+
+    def testeDeProfundidade(self):
+        submarino.ressetaCoordenadas()
+        self.assertEqual(0, submarino.Z)
+        self.assertEqual(0, submarino.calculaUP())
+        self.assertEqual(-1, submarino.calculaDOWN())
+        self.assertEqual(-2, submarino.calculaDOWN())
+        self.assertEqual(-3, submarino.calculaDOWN())
+        self.assertEqual(-2, submarino.calculaUP())
+
+
+    def testeCalculaAvancoNORTE(self):
+        submarino.ressetaCoordenadas()
+        submarino.direcao = 'NORTE'
+        submarino.calculaAvanco()
+        self.assertEqual(0, submarino.X)
+        self.assertEqual(1, submarino.Y)
+        submarino.calculaAvanco()
+        self.assertEqual(0, submarino.X)
+        self.assertEqual(2, submarino.Y)
+
+
+    def testeCalculaAvancoSUL(self):
+        submarino.ressetaCoordenadas()
+        submarino.direcao = 'SUL'
+        submarino.calculaAvanco()
+        self.assertEqual(0, submarino.X)
+        self.assertEqual(-1, submarino.Y)
+        submarino.calculaAvanco()
+        self.assertEqual(0, submarino.X)
+        self.assertEqual(-2, submarino.Y)
+
+
+    def testeCalculaAvancoLESTE(self):
+        submarino.ressetaCoordenadas()
+        submarino.direcao = 'LESTE'
+        submarino.calculaAvanco()
+        self.assertEqual(1, submarino.X)
+        self.assertEqual(0, submarino.Y)
+        submarino.calculaAvanco()
+        self.assertEqual(2, submarino.X)
+        self.assertEqual(0, submarino.Y)
+
+
+    def testeCalculaAvancoOESTE(self):
+        submarino.ressetaCoordenadas()
+        submarino.direcao = 'OESTE'
+        submarino.calculaAvanco()
+        self.assertEqual(-1, submarino.X)
+        self.assertEqual(0, submarino.Y)
+        submarino.calculaAvanco()
+        self.assertEqual(-2, submarino.X)
+        self.assertEqual(0, submarino.Y)
+
+
+if __name__ == '__main__':
+    unittest.main()
