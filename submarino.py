3f43dbd12dc9c66d6e487a023580e40dd2e75ebb
Red Bill <redbillb@gmail.com>
2018-08-23 22:22:27 -0300

    Refatorando o Codigo

--- submarino.py
+++ submarino.py
@@ -7,83 +7,105 @@ class Submarino:
 		self.Y = 0
 		self.Z = 0
 		self.direcao = 'NORTE'
+		self.direcoes=['NORTE','LESTE','SUL','OESTE']
 
-	def retornaPosicaoFinal(self,comandos):
+	def ressetaCoordenadas(self):
 		self.__init__()
+		return f'{self.X} {self.Y} {self.Z} {self.direcao}'
 
+	def calculaCoordenada(self,comandos,resseta):
+		if resseta:
+			self.ressetaCoordenadas()
 		for caractere in comandos:
-			if caractere in ['R','L']:
-				self.__calculaDirecao(caractere)
+			if caractere == 'L':
+				self.calculaLEFT()
 
-			elif caractere in ['U','D']:
-				self.__calcuDeslocamentoZ(caractere)
+			elif caractere == 'R':
+				self.calculaRIGTH()
+
+			elif caractere == 'U':
+				self.calculaUP()
+
+			elif caractere == 'D':
+				self.calculaDOWN()
 
 			elif caractere == 'M':
-				self.__calculaAvanco()
+				self.calculaAvanco()
 
 			else:
 				pass
 
 		return f'{self.X} {self.Y} {self.Z} {self.direcao}'
 
-	def __calculaDirecao(self,caractere):
-		direcoes=['NORTE','LESTE','SUL','OESTE']
-		
-		for indice in range(len(direcoes)):
-			if self.direcao == direcoes[indice]:
-				indiceDirecoes = indice
+	def calculaIndiceDirecaoAtual(self):
+		for indice in range(len(self.direcoes)):
+			if self.direcao == self.direcoes[indice]:
+				return indice
 
-		if caractere == 'L':
-			indiceDirecoes += 1
-		
-		if caractere == 'R':
-			indiceDirecoes -= 1
-		
-		if indiceDirecoes == -1:
-			indiceDirecoes = 3
+	def calculaLEFT(self):
+		indiceDirecoes = self.calculaIndiceDirecaoAtual() + 1
+		indiceDirecoes = self.ajustaIndiceDirecoes(indiceDirecoes)
+		self.direcao = self.direcoes[indiceDirecoes]
+		return self.direcao
 
-		if indiceDirecoes == 4:
-			indiceDirecoes = 0
+	def calculaRIGTH(self):
+		indiceDirecoes = self.calculaIndiceDirecaoAtual() - 1
+		indiceDirecoes = self.ajustaIndiceDirecoes(indiceDirecoes)
+		self.direcao = self.direcoes[indiceDirecoes]
+		return self.direcao
+
+	def ajustaIndiceDirecoes(self,indice):
+		if indice == -1:
+			indice = 3
+
+		if indice == 4:
+			indice = 0
+
+		return indice
 		
-		self.direcao = direcoes[indiceDirecoes]
-			
-	def __calcuDeslocamentoZ(self,caractere):
-		if caractere == 'U':
+	def calculaUP(self):
+		if self.Z < 0:
 			self.Z += 1
+		return self.Z
 		
-		if caractere == 'D':
-			self.Z -= 1
+	def calculaDOWN(self):
+		self.Z -= 1
+		return self.Z
 		
-	def __calculaAvanco(self):
+	def calculaAvanco(self):
 		if self.direcao == 'NORTE':
 			self.Y += 1
+			return self.Y
 		
 		if self.direcao == 'SUL':
 			self.Y -= 1
-		
+			return self.Y
+
 		if self.direcao == 'LESTE':
 			self.X -= 1
+			return self.X
 		
 		if self.direcao == 'OESTE':
 			self.X += 1
+			return self.X
 
 #testes
 #OBS: a cada teste a posição volta a ser a inicial
 
 submarino = Submarino()
-assert '0 0 0 NORTE' == submarino.retornaPosicaoFinal('')
-assert '0 0 0 NORTE' == submarino.retornaPosicaoFinal('a')
-assert '0 0 0 LESTE' == submarino.retornaPosicaoFinal('L')
-assert '0 0 0 OESTE' == submarino.retornaPosicaoFinal('R')
-assert '0 0 1 NORTE' == submarino.retornaPosicaoFinal('U')
-assert '0 0 -1 NORTE' == submarino.retornaPosicaoFinal('D')
-assert '0 0 -4 NORTE' == submarino.retornaPosicaoFinal('DDDD')
-assert '0 0 3 NORTE' == submarino.retornaPosicaoFinal('UUU')
-assert '0 0 3 NORTE' == submarino.retornaPosicaoFinal('UUUUUDD')
-assert '0 1 0 NORTE' == submarino.retornaPosicaoFinal('M')
-assert '0 5 0 NORTE' == submarino.retornaPosicaoFinal('MMMMM')
-assert '0 -4 0 SUL' == submarino.retornaPosicaoFinal('RRMMMM')
-assert '0 0 0 NORTE' == submarino.retornaPosicaoFinal('RRRR')
-assert '0 0 0 NORTE' == submarino.retornaPosicaoFinal('LLLL')
+assert '0 0 0 NORTE' == submarino.calculaCoordenada('',True)
+assert '0 0 0 NORTE' == submarino.calculaCoordenada('a',True)
+assert '0 0 0 LESTE' == submarino.calculaCoordenada('L',True)
+assert '0 0 0 OESTE' == submarino.calculaCoordenada('R',True)
+assert '0 0 0 NORTE' == submarino.calculaCoordenada('U',True)
+assert '0 0 -1 NORTE' == submarino.calculaCoordenada('D',True)
+assert '0 0 -4 NORTE' == submarino.calculaCoordenada('DDDD',True)
+assert '0 0 0 NORTE' == submarino.calculaCoordenada('DDDUUU',True)
+assert '0 0 -2 NORTE' == submarino.calculaCoordenada('UUUUDD',True)
+assert '0 1 0 NORTE' == submarino.calculaCoordenada('M',True)
+assert '0 5 0 NORTE' == submarino.calculaCoordenada('MMMMM',True)
+assert '0 -4 0 SUL' == submarino.calculaCoordenada('RRMMMM',True)
+assert '0 0 0 NORTE' == submarino.calculaCoordenada('RRRR',True)
+assert '0 0 0 NORTE' == submarino.calculaCoordenada('LLLL',True)
 
-assert '2 3 -2 SUL' == submarino.retornaPosicaoFinal('RMMLMMMDDLL')
+assert '2 3 -2 SUL' == submarino.calculaCoordenada('RMMLMMMDDLL',True)
